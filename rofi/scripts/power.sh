f0b6ea7ab322020d20980cc2eec5321b8e6db8ed
Rithul Kamesh <hi@rithul.dev>
2023-11-04 18:38:54 +0530

    add: i3blocks config

--- rofi/scripts/power.sh
+++ rofi/scripts/power.sh
@@ -1,13 +1,12 @@
 #! /bin/sh
 
-chosen=$(printf "  Power Off\n  Restart\n  Suspend\n  Hibernate\n  Log Out\n  Lock" | rofi -dmenu -i -theme-str '@import "power.rasi"')
+chosen=$(printf "  Power Off\n  Restart\n  Suspend\n  Hibernate\n󰍃  Log Out" | rofi -dmenu -i -theme-str '@import "power.rasi"')
 
 case "$chosen" in
 	"  Power Off") poweroff ;;
 	"  Restart") reboot ;;
 	"  Suspend") systemctl suspend-then-hibernate ;;
 	"  Hibernate") systemctl hibernate ;;
-	"  Log Out") hyprctl dispatch exit;;
-	"  Lock") sh ~/.config/hypr/scripts/lock.sh;;
+	"󰍃  Log Out") pkill -u rithulk;;
 	*) exit 1 ;;
 esac
--- scripts/battery.sh
+++ scripts/battery.sh
@@ -2,7 +2,7 @@
 
 BAT=$(acpi -b | grep -E -o '[0-9][0-9]?%')
 
-echo "BAT: $BAT"
+echo "🔋: $BAT"
 
 [ ${BAT%?} -le 5 ] && exit 33
 [ ${BAT%?} -le 20 ] && echo "#FF8000"
